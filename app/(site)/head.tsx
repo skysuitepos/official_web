f5f1175021b240406c0e5f2918b6ca8b399e5d63
aSameeraTennakoon <a.sameeratennakoon@gmail.com>
2024-05-23 23:40:29 +0530

    first commit

---
+++ app/(site)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> SkyPOS 360 | Mobile first Cloud POS system</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Built with Next.js and TypeScript" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
---
+++ components/Header/index.tsx
@@ -0,0 +1,167 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import ThemeToggler from "./ThemeToggler";
+import menuData from "./menuData";
+
+const Header = () => {
+  const [navigationOpen, setNavigationOpen] = useState(false);
+  const [dropdownToggler, setDropdownToggler] = useState(false);
+  const [stickyMenu, setStickyMenu] = useState(false);
+
+  const pathUrl = usePathname();
+
+  // Sticky menu
+  const handleStickyMenu = () => {
+    if (window.scrollY >= 80) {
+      setStickyMenu(true);
+    } else {
+      setStickyMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleStickyMenu);
+  });
+
+  return (
+    <header
+      className={`fixed left-0 top-0 z-99999 w-full py-7 ${stickyMenu
+          ? "bg-white !py-4 shadow transition duration-100 dark:bg-black"
+          : ""
+        }`}
+    >
+      <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
+        <div className="flex w-full items-center justify-between xl:w-1/4">
+          <a href="/">
+            <Image
+              src="/images/logo/logo-dark.svg"
+              alt="logo"
+              width={119.03}
+              height={30}
+              className="hidden w-full dark:block"
+            />
+            <Image
+              src="/images/logo/logo-light.svg"
+              alt="logo"
+              width={119.03}
+              height={30}
+              className="w-full dark:hidden"
+            />
+          </a>
+
+          {/* <!-- Hamburger Toggle BTN --> */}
+          <button
+            aria-label="hamburger Toggler"
+            className="block xl:hidden"
+            onClick={() => setNavigationOpen(!navigationOpen)}
+          >
+            <span className="relative block h-5.5 w-5.5 cursor-pointer">
+              <span className="absolute right-0 block h-full w-full">
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${!navigationOpen ? "!w-full delay-300" : "w-0"
+                    }`}
+                ></span>
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${!navigationOpen ? "delay-400 !w-full" : "w-0"
+                    }`}
+                ></span>
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${!navigationOpen ? "!w-full delay-500" : "w-0"
+                    }`}
+                ></span>
+              </span>
+              <span className="du-block absolute right-0 h-full w-full rotate-45">
+                <span
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${!navigationOpen ? "!h-0 delay-[0]" : "h-full"
+                    }`}
+                ></span>
+                <span
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${!navigationOpen ? "!h-0 delay-200" : "h-0.5"
+                    }`}
+                ></span>
+              </span>
+            </span>
+          </button>
+          {/* <!-- Hamburger Toggle BTN --> */}
+        </div>
+
+        {/* Nav Menu Start   */}
+        <div
+          className={`invisible h-0 w-full items-center justify-between xl:visible xl:flex xl:h-auto xl:w-full ${navigationOpen &&
+            "navbar !visible mt-4 h-auto max-h-[400px] rounded-md bg-white p-7.5 shadow-solid-5 dark:bg-blacksection xl:h-auto xl:p-0 xl:shadow-none xl:dark:bg-transparent"
+            }`}
+        >
+          <nav>
+            <ul className="flex flex-col gap-5 xl:flex-row xl:items-center xl:gap-10">
+              {menuData.map((menuItem, key) => (
+                <li key={key} className={menuItem.submenu && "group relative"}>
+                  {menuItem.submenu ? (
+                    <>
+                      <button
+                        onClick={() => setDropdownToggler(!dropdownToggler)}
+                        className="flex cursor-pointer items-center justify-between gap-3 hover:text-primary"
+                      >
+                        {menuItem.title}
+                        <span>
+                          <svg
+                            className="h-3 w-3 cursor-pointer fill-waterloo group-hover:fill-primary"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                          </svg>
+                        </span>
+                      </button>
+
+                      <ul
+                        className={`dropdown ${dropdownToggler ? "flex" : ""}`}
+                      >
+                        {menuItem.submenu.map((item, key) => (
+                          <li key={key} className="hover:text-primary">
+                            <Link href={item.path || "#"}>{item.title}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </>
+                  ) : (
+                    <Link
+                      href={`${menuItem.path}`}
+                      className={
+                        pathUrl === menuItem.path
+                          ? "text-primary hover:text-primary"
+                          : "hover:text-primary"
+                      }
+                    >
+                      {menuItem.title}
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          <div className="mt-7 flex items-center gap-6 xl:mt-0">
+            <ThemeToggler />
+            <Link
+              href="https://wa.me/94765783980?text=I'm%20interested%20in%20your%20POS%20for%20system"
+              className="flex items-center justify-center rounded-full bg-whatsApp px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
+            >
+              <svg width="24" height="24" viewBox="0 0 360 362" className="w-5 h-5 mr-2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M307.546 52.5655C273.709 18.685 228.706 0.0171895 180.756 0C81.951 0 1.53846 80.404 1.50408 179.235C1.48689 210.829 9.74646 241.667 25.4319 268.844L0 361.736L95.0236 336.811C121.203 351.096 150.683 358.616 180.679 358.625H180.756C279.544 358.625 359.966 278.212 360 179.381C360.017 131.483 341.392 86.4547 307.546 52.5741V52.5655ZM180.756 328.354H180.696C153.966 328.346 127.744 321.16 104.865 307.589L99.4242 304.358L43.034 319.149L58.0834 264.168L54.5423 258.53C39.6304 234.809 31.749 207.391 31.7662 179.244C31.8006 97.1036 98.6334 30.2707 180.817 30.2707C220.61 30.2879 258.015 45.8015 286.145 73.9665C314.276 102.123 329.755 139.562 329.738 179.364C329.703 261.513 262.871 328.346 180.756 328.346V328.354ZM262.475 216.777C257.997 214.534 235.978 203.704 231.869 202.209C227.761 200.713 224.779 199.966 221.796 204.452C218.814 208.939 210.228 219.029 207.615 222.011C205.002 225.002 202.389 225.372 197.911 223.128C193.434 220.885 179.003 216.158 161.891 200.902C148.578 189.024 139.587 174.362 136.975 169.875C134.362 165.389 136.7 162.965 138.934 160.739C140.945 158.728 143.412 155.505 145.655 152.892C147.899 150.279 148.638 148.406 150.133 145.423C151.629 142.432 150.881 139.82 149.764 137.576C148.646 135.333 139.691 113.287 135.952 104.323C132.316 95.5909 128.621 96.777 125.879 96.6309C123.266 96.5019 120.284 96.4762 117.293 96.4762C114.302 96.4762 109.454 97.5935 105.346 102.08C101.238 106.566 89.6691 117.404 89.6691 139.441C89.6691 161.478 105.716 182.785 107.959 185.776C110.202 188.767 139.544 234.001 184.469 253.408C195.153 258.023 203.498 260.782 210.004 262.845C220.731 266.257 230.494 265.776 238.212 264.624C246.816 263.335 264.71 253.786 268.44 243.326C272.17 232.866 272.17 223.893 271.053 222.028C269.936 220.163 266.945 219.037 262.467 216.794L262.475 216.777Z" fill="#111B21" />
+              </svg>
+              Chat on WhatsApp
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+// w-full delay-300
+
+export default Header;
